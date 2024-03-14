f87181de7e692ae3e8b4fe81a6d6a64a243b82f3
raushanranjankumar <raushan.ranjan.kumar123@gmail.com>
2024-03-14 18:43:58 +0530

    Code changes for practice questions

---
+++ example/src/example/SourceProgram.java
@@ -0,0 +1,10 @@
+package example;
+
+public class SourceProgram {
+
+		  public static void main(String[] args) {
+		    float a = 123456789.0f;
+		    int b = (int) a;
+		        System.out.println(b);
+		  }
+}
